65b173a09e6cbb0370bd61bbdd7cfce18978d1e2
Hardik Jaysukhbhai Dudhrejia <hr208032@dal.ca>
2020-02-15 00:01:00 -0400

    Added navigation bar

--- app/src/main/java/com/dal/ideasmeetfinance/MainActivity.java
+++ app/src/main/java/com/dal/ideasmeetfinance/MainActivity.java
@@ -3,27 +3,61 @@ package com.dal.ideasmeetfinance;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
+    private String userID;
     private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createSignInIntent();
+        createSignInIntent(); 
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.main);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open,R.string.close);
+        mDrawerlayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        View headerLayout = navigationView.getHeaderView(0);
+        TextView txt_email =  headerLayout.findViewById(R.id.userEmail);
+        TextView txt_username =  headerLayout.findViewById(R.id.userName);
+        SharedPreferences sp = this.getSharedPreferences("Login", MODE_PRIVATE);
+
+        String user_name = sp.getString("UserName", null);
+        String user_email = sp.getString("UserEmail", null);
+        txt_email.setText(user_email);
+        txt_username.setText(user_name);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void createSignInIntent(){
@@ -59,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
                 SharedPreferences.Editor ed = sp.edit();
                 ed.putString("UserName", user.getDisplayName());
                 ed.putString("UserEmail", user.getEmail());
+                ed.apply();
 
 
             } else {
